d10c97700e859dad7fbcc5846c7197a8b9f1c9d1
Senails <seffele@gmail.com>
2023-08-29 05:14:15 +0300

    add logic for chatbot

--- src/ChatBotWidget/Utils/GetBotAnswer.ts
+++ src/ChatBotWidget/Utils/GetBotAnswer.ts
@@ -7,14 +7,25 @@ export type TBotAnswer = {
 }
 
 export async function GetBotAnswer(userMessae: string):Promise<TBotAnswer>{
-    await Sleep(2500);
+    await Sleep(1500);
+    const fastMessagesForUser = ["Заказать пиццу", "Установить будильник", "Вывести погоду"];
+    const botMessage = {
+        id: Date.now(),
+        text: "Привет! Что я могу для Вас сделать?",
+        authorname: "ChatBot",
+        date:""
+    }
+
+    if (userMessae === "Заказать пиццу"){ 
+        botMessage.text="Я закажу пиццу. Что то еще?"
+    }else if (userMessae === "Установить будильник"){ 
+        botMessage.text="Я установлю будильник. Что то еще?"
+    }else if (userMessae === "Вывести погоду"){ 
+        botMessage.text="Я покажу погоду. Что то еще?"
+    }else {botMessage.text="Я постараюсь сделать все что моих силах. Что то еще?" }
+
     return {
-        botMessage : {
-            id: Date.now(),
-            text: "Привет! Что я могу для Вас сделать?",
-            authorname: "ChatBot",
-            date:""
-        },
-        fastMessagesForUser: ["Заказать пиццу", "Установить будильник", "Вывести погоду"]
+        botMessage,
+        fastMessagesForUser
     }
 }
